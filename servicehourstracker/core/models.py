2d1a0b1015f090b8481fe0cb71d0582d74fba0f7
William Gocheco <isaerwilliamvicsongocheco@gmail.com>
2025-07-16 13:39:57 +0800

    added db models for events and through tables for participation

--- servicehourstracker/core/models.py
+++ servicehourstracker/core/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    id_number = models.CharField(max_length=6)
+    id_number = models.CharField(max_length=6, unique=True)
     
     required_service_hours = models.FloatField(default=20.0)  
     completed_service_hours = models.FloatField(default=0.0)
@@ -17,3 +15,22 @@ class OrgProfile(models.Model):
 
 class OAAProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=50)
+    service_hours = models.IntegerField()
+    number_of_students = models.IntegerField()
+    role_descriptions = models.TextField()
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    organizer = models.ForeignKey(OrgProfile, on_delete=models.CASCADE, related_name='events')
+    approved = models.BooleanField(default=False)
+    students = models.ManyToManyField(StudentProfile, through='Participation', related_name='events')
+    
+class Participation(models.Model):
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    attended = models.BooleanField(default=False)
+
